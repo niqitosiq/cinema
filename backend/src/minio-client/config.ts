a88eafd7cc38946cc8df784120bdf95ab26dd13a
DanielTenebris <danieltenebris@gmail.com>
2020-11-15 07:34:02 +0000

    :recycle: Fix minio config errors

--- backend/src/minio-client/config.ts
+++ backend/src/minio-client/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  MINIO_ENDPOINT: 'http://s3.cinema-hack.rednite.ru/',
+  MINIO_ENDPOINT: 's3.cinema-hack.rednite.ru',
   MINIO_PORT: 9000,
   MINIO_ACCESSKEY: process.env.MINIO_ACCESSKEY,
   MINIO_SECRETKEY: process.env.MINIO_SECRETKEY,
