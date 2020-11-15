e51ba43720ac933eb59f3b538b7bb11dc35e77b3
DanielTenebris <danieltenebris@gmail.com>
2020-11-15 12:02:34 +0000

    :arrow_up: Add InteractiveModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClipModule } from './clip/clip.module';
 import { MinioClientModule } from './minio-client/minio-client.module';
 import { UploaderModule } from './uploader/uploader.module';
+import { InteractiveModule } from './interactive/interactive.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ClipModule, MinioClientModule, UploaderModule],
+  imports: [TypeOrmModule.forRoot(), ClipModule, MinioClientModule, UploaderModule, InteractiveModule],
   controllers: [],
   providers: [],
 })
